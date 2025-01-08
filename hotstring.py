9c81f9bbf407a3ae8de1a60cb9af03dd2054418a
beskep <beskep@naver.com>
2025-01-08 14:52:01 +0900

    feat: hotstring formatting

--- hotstring.py
+++ hotstring.py
@@ -1,39 +1,62 @@
+from __future__ import annotations
+
 import tomllib
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 
-class Hotstring:
+class Hotstrings:
     def __init__(
         self,
         v: dict[str, str],
         /,
-        option='?*',
+        option: str = '?*',
         prefix: str = '\\',
         suffix: str | None = None,
-    ):
+    ) -> None:
         self.v = v
         self.option = option
         self.prefix = prefix
         self.suffix = suffix or ''
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         for src, dst in self.v.items():
             yield f':{self.option}:{self.prefix}{src}{self.suffix}::{dst}'
 
 
+class HotstringGroups:
+    def __init__(
+        self,
+        v: dict[str, dict[str, str]],
+        /,
+        prefix: str = '\\',
+        suffix: str | None = None,
+    ) -> None:
+        self.v = v
+        self.prefix = prefix
+        self.suffix = suffix or ''
+
+    def __iter__(self) -> Iterator[str | None]:
+        for group, v in self.v.items():
+            yield f'; {group}'
+            yield from Hotstrings(
+                v,
+                option='?*C' if group in {'greek', 'arrow'} else '?*',
+                suffix=None if group == 'subscript_superscript' else '\\',
+            )
+            yield None
+
+    def __str__(self) -> str:
+        return '\n'.join(f'    {x}' if x else '' for x in self)
+
+
 if __name__ == '__main__':
     config = Path('hotstring.toml')
-    script = Path('hotstring.ahk')
+    template = Path('script.template.ahk')
+    script = Path('script.ahk')
 
-    data: dict[str, dict[str, str]] = tomllib.loads(config.read_text())
-
-    with script.open('w') as f:
-        for group, hotstring in data.items():
-            f.write(f'; {group}\n')
-
-            option = '?*C' if group in {'greek', 'arrow'} else '?*'
-            suffix = None if group == 'subscript_superscript' else '\\'
-            for line in Hotstring(hotstring, option=option, suffix=suffix):
-                f.write(f'{line}\n')
-
-            f.write('\n')
+    hg = HotstringGroups(tomllib.loads(config.read_text()))
+    script.write_text(template.read_text().replace(';{hotstrings}', str(hg)))
---
+++ script.template.ahk
@@ -0,0 +1,85 @@
+#Requires AutoHotkey v2.0
+
+control := true
+hotstrings := true
+wheel_down := 'None' ; {'None', '{LButton}', '{Space}'}
+
+TimeoutTrayTip(text, title := '', option := '', time := 1000) {
+    TrayTip text
+    SetTimer TrayTip, -time
+}
+
+^!+1::
+{
+    global control
+    control := !control
+    TimeoutTrayTip('Control: ' . (control ? "On" : "Off"))
+}
+
+^!+2::
+{
+    global hotstrings
+    hotstrings := !hotstrings
+    TimeoutTrayTip('Hotstrings: ' . (hotstrings ? "On" : "Off"))
+}
+
+^!+3::
+{
+    global wheel_down
+    wheel_down := 'None'
+    TimeoutTrayTip('WheelDown: ' . wheel_down)
+}
+
+^!+4::
+{
+    global wheel_down
+    wheel_down := wheel_down = '{LButton}' ? 'None' : '{LButton}'
+    TimeoutTrayTip('WheelDown: ' . wheel_down)
+}
+
+^!+5::
+{
+    global wheel_down
+    wheel_down := wheel_down = '{Space}' ? 'None' : '{Space}'
+    TimeoutTrayTip('WheelDown: ' . wheel_down)
+}
+
+#HotIf control
+{
+    ; Capslock
+    +Capslock::Capslock
+    Capslock::Ctrl
+
+    ; Mouse
+    XButton2:: Send '^{w}'
+
+    ; Alt control
+    !i:: Send '{Up}'
+    !k:: Send '{Down}'
+    !j:: Send '{Left}'
+    !l:: Send '{Right}'
+
+    ; Volume
+    ^!PgDn:: Send '{Volume_Down}'
+    ^!PgUp:: Send '{Volume_Up}'
+
+    ; Hotstring
+    :?*:\today\::
+    {
+        Send FormatTime(, "yyyy-MM-dd")
+    }
+    :?*:\now\::
+    {
+        Send FormatTime(, "yyyy-MM-dd HH:mm:ss")
+    }
+}
+
+#HotIf hotstrings
+{
+;{hotstrings}
+}
+
+#HotIf wheel_down != 'None'
+{
+    WheelDown:: Send wheel_down
+}
